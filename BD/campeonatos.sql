40499b012a6391081110132799d937801e4b8c5a
ivan <andresivan0807@gmail.com>
2025-12-12 21:20:52 -0500

    a

--- BD/campeonatos.sql
+++ BD/campeonatos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-12-2025 a las 02:20:08
+-- Tiempo de generación: 13-12-2025 a las 03:20:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
